941de4f662149d4d43be39ce9bde6ac10d787d13
Bruno Coudoin <bruno.coudoin@gcompris.net>
2015-04-06 19:20:45 +0200

    core, checkForVoices() now check data is loaded instead of rcc file

--- src/core/core.js
+++ src/core/core.js
@@ -177,9 +177,7 @@ function showDownloadDialog(parent, properties) {
  */
 function checkForVoices(parent)
 {
-    if (!GCompris.DownloadManager.haveLocalResource(
-            GCompris.DownloadManager.getVoicesResourceForLocale(
-                    GCompris.ApplicationSettings.locale))) {
+    if (!GCompris.DownloadManager.areVoicesRegistered()) {
         showMessageDialog(parent,
                 qsTr("Missing sound files!") + '\n'
                 + qsTr("This activity makes use of language specific sound files, that are not yet installed on your system.")
